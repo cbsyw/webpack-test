3d3ac4b614580ba00ddb39ec772edabad45456e9
Chris Warren <101680158+cbsyw@users.noreply.github.com>
2024-07-04 22:17:08 -0400

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-	filename: 'main.js',
-	path: path.resolve(__dirname, 'dist'),
-	clean:true,
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 
+        use: ['style-loader', 'css-loader'], 
+      },
+    ],
   },
   plugins: [
-	  new HtmlWebpackPlugin({
-		  template: './src/index.html',
-	  }),
-	],
-	mode: 'development',
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
 };
+
